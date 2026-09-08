f622c709cda415c63d52fed72037a45881e77664
Heather Succio, MSEd. <hsuccio@gmail.com>
2026-09-08 08:42:37 -0500

    Update results-ui.js

--- quiz/js/results-ui.js
+++ quiz/js/results-ui.js
@@ -11,9 +11,9 @@ export function renderResultsScreen(primaryKey, secondaryKey, tertiaryKey) {
   if (bannerActions) {
       if (!document.getElementById('btnCopyLink')) {
           bannerActions.insertAdjacentHTML('afterbegin', `
-              <button onclick="window.print()" class="beta-tag action-btn">PRINT</button>
-              <button id="btnCopyLink" class="beta-tag action-btn">COPY</button>
-              <button id="btnResetQuiz" class="beta-tag action-btn">RETAKE</button>
+              <button onclick="window.print()" class="beta-tag" style="cursor: pointer; border: none;">PRINT</button>
+              <button id="btnCopyLink" class="beta-tag" style="cursor: pointer; border: none;">COPY</button>
+              <button id="btnResetQuiz" class="beta-tag" style="cursor: pointer; border: none;">RETAKE</button>
           `);
       }
   }
